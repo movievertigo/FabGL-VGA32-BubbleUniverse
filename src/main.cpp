6c0a608bfad81af4d6f217e84ca3615fea2d41e9
Movie Vertigo <movievertigo@gmail.com>
2024-06-20 01:08:35 +0100

    VGA direct rendering at 1024x768

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,14 @@
 #include "fabgl.h"
 #include <esp_task_wdt.h>
 
-#define DS_320x240_60Hz   "\"320x240@60d\" 12.14 320 352 368 400 240 245 248 253 -HSync -VSync DoubleScan"
-#define DS_320x240_75Hz   "\"320x240@75d\" 15.48 320 352 376 408 240 245 248 253 -HSync -VSync DoubleScan"
-#define DS_320x240_85Hz   "\"320x240@85d\" 17.89 320 352 384 416 240 244 248 253 -HSync -VSync DoubleScan"
-#define DS_320x240_100Hz "\"320x240@100d\" 21.46 320 352 392 424 240 244 248 253 -HSync -VSync DoubleScan"
-#define DS_320x240_120Hz "\"320x240@120d\" 26.23 320 352 400 432 240 244 249 253 -HSync -VSync DoubleScan"
-#define DS_320x240_150Hz "\"320x240@150d\" 34.00 320 352 416 448 240 243 249 253 -HSync -VSync DoubleScan"
-#define SS_320x240_60Hz    "\"320x240@60\" 06.07 320 352 368 400 240 245 248 253 -HSync -VSync"
-#define SS_320x240_75Hz    "\"320x240@75\" 07.74 320 352 376 408 240 245 248 253 -HSync -VSync"
-#define SS_320x240_85Hz    "\"320x240@85\" 08.95 320 352 384 416 240 244 248 253 -HSync -VSync"
-#define SS_320x240_100Hz  "\"320x240@100\" 10.73 320 352 392 424 240 244 248 253 -HSync -VSync"
-#define SS_320x240_120Hz  "\"320x240@120\" 13.12 320 352 400 432 240 244 249 253 -HSync -VSync"
-#define SS_320x240_150Hz  "\"320x240@150\" 17.00 320 352 416 448 240 243 249 253 -HSync -VSync"
-#define SS_320x240_160Hz  "\"320x240@160\" 18.14 320 352 416 448 240 243 250 253 -HSync -VSync"
+#define SS_640x480_135Hz  "\"640x480@135Hz\" 60 640 680 744 848  480 483 487 523 -HSync -VSync"
+#define SS_800x600_90Hz  "\"800x600@90Hz\" 60 800 840 968 1056 600 601 605 628 -HSync -VSync"
+#define SS_1024x768_60Hz "\"1024x768@60Hz\" 62.5 1024 1072 1176 1308 768 771 775 795 -HSync -VSync"
 
-
-const char* screenMode = SS_320x240_160Hz;
-const int pixelSize = 240;
+const char* screenMode = SS_1024x768_60Hz;
+const int width = 1024;
+const int pixelSize = 768;
+const int xOffset = (width - pixelSize)/2;
 const float pSize = 0.25f * pixelSize;
 const float pOffset = 0.5f * pixelSize;
 const int sinTableSize = 1<<13;
@@ -26,9 +17,8 @@ const float sinTableScale = sinTableSize / (2*PI) / pSize;
 float* sinTable;
 uint8_t* offscreen;
 
-fabgl::VGAController displayController;
-fabgl::Canvas canvas(&displayController);
-fabgl::Bitmap bitmap;
+fabgl::VGADirectController displayController;
+TaskHandle_t  mainTaskHandle;
 SemaphoreHandle_t semaphore;
 
 const int curveShift = 8;
@@ -36,12 +26,29 @@ const int curveCount = 1<<curveShift;
 const int curveStep = 4;
 const int iterationShift = 9;
 const int iterationCount = 1<<iterationShift;
+
+const int slotSizeMultiplier = 1.55f * iterationCount * curveCount / (curveStep * pixelSize);
+const int minSlotSize = 2;
+
+struct Slot
+{
+    uint8_t capacity;
+    uint8_t count;
+    uint16_t* data;
+};
+
+Slot* writeSlots = nullptr;
+Slot* displaySlots = nullptr;
+Slot* slotsA = nullptr;
+Slot* slotsB = nullptr;
+
 float speed = 0.00015f;
 
 const float ang1inc = pSize * curveStep * 2 * PI / 235;
 const float ang2inc = pSize * curveStep;
 float animTime;
 
+bool vsync = false;
 int frame = 0;
 int totalTime = 0;
 
@@ -67,24 +74,73 @@ void createSinTable()
     }
 }
 
+void createSlots()
+{
+    slotsA = (Slot*)malloc(pixelSize * sizeof(Slot));
+    slotsB = (Slot*)malloc(pixelSize * sizeof(Slot));
+    for (int i = 0; i < pixelSize; ++i)
+    {
+        for (int bank = 0; bank < 2; ++bank)
+        {
+            Slot* slots = bank ? slotsB : slotsA;
+
+            int r = pixelSize>>1;
+            int slotSize = slotSizeMultiplier * sqrt(r*r - (i+0.5f-r)*(i+0.5f-r)) / r;
+            slotSize = slotSize < minSlotSize ? minSlotSize : slotSize;
+            slots[i].capacity = slotSize;
+            slots[i].count = 0;
+            slots[i].data = (uint16_t*)malloc(slotSize * sizeof(uint16_t));
+            if (slots[i].data == nullptr)
+            {
+                Serial.println("Failed to allocate memory for slot");
+            }
+        }
+    }
+
+    writeSlots = slotsA;
+    displaySlots = slotsB;
+}
+
+void clearSlots()
+{
+    for (int i = 0; i < pixelSize; ++i)
+    {
+        writeSlots[i].count = 0;
+    }
+}
+
+void IRAM_ATTR drawScanline(void * arg, uint8_t * dest, int scanLine)
+{
+    if (displaySlots)
+    {
+        memset(dest, displayController.createBlankRawPixel(), width);
+        const Slot& slot = displaySlots[scanLine];
+        for (int i = 0; i < slot.count; ++i)
+        {
+            auto pixel = displayController.createBlankRawPixel() | (slot.data[i] >> 10);
+            VGA_PIXELINROW(dest, xOffset + (slot.data[i] & ((1<<10)-1))) = pixel;
+        }
+        
+        if (scanLine == pixelSize-1)
+        {
+            vTaskNotifyGiveFromISR(mainTaskHandle, NULL); // Trigger vsync
+        }
+    }
+}
+
 void setup(void)
 {
     Serial.begin(115200);
 
+    mainTaskHandle = xTaskGetCurrentTaskHandle();
+
     displayController.begin();
-    displayController.setResolution(screenMode, pixelSize, pixelSize, false);
+    displayController.setScanlinesPerCallBack(1);
+    displayController.setDrawScanlineCallback(drawScanline);
+    displayController.setResolution(screenMode, width, pixelSize, false);
 
-    canvas.selectFont(&fabgl::FONT_8x8);
-    canvas.setBrushColor(Color::Black);
-    canvas.setPenColor(Color::White);
-
-    offscreen = (uint8_t*)malloc(pixelSize * pixelSize * sizeof(uint8_t));
     createSinTable();
-
-    bitmap.width = pixelSize;
-    bitmap.height = pixelSize;
-    bitmap.data = offscreen;
-    bitmap.format = PixelFormat::RGBA2222;
+    createSlots();
 
     semaphore = xSemaphoreCreateBinary();
 }
@@ -110,7 +166,24 @@ void bubbleUniverseHalf(int section)
             y = sinTable[sinoffset1+sinTableSize/4] + sinTable[sinoffset2+sinTableSize/4];
             const uint8_t green = j>>(iterationShift-2);
             const uint8_t blue = (7-(red+green))>>1;
-            offscreen[(int)y*pixelSize + (int)x] = red + (green << 2) + (blue << 4);
+            Slot& slot = writeSlots[(int)y];
+            if (slot.count < slot.capacity)
+            {
+                bool found = false;
+                for (int check = 0; check < slot.count; ++check)
+                {
+                    if ((slot.data[check] & ((1<<10)-1)) == (int)x)
+                    {
+                        slot.data[check] = (int)x + (red << 10) + (green << 12) + (blue << 14);
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    slot.data[slot.count++] = (int)x + (red << 10) + (green << 12) + (blue << 14);
+                }
+            }
         }
 
         ang1Start += ang1inc;
@@ -132,13 +205,14 @@ void loop()
 
     animTime = start * speed /1000;
 
-    memset(offscreen, 0, pixelSize * pixelSize * sizeof(uint8_t));
+    clearSlots();
 
     xTaskCreatePinnedToCore(halfTask, "Half", 1000, nullptr, configMAX_PRIORITIES - 1, nullptr, 0);
     bubbleUniverseHalf(1);
     xSemaphoreTake(semaphore, portMAX_DELAY);
 
-    canvas.clear();
-    canvas.drawBitmap(0,0, &bitmap);
-    canvas.waitCompletion(true);
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // Wait for vsync
+
+    writeSlots = (writeSlots == slotsA ? slotsB : slotsA);
+    displaySlots = (displaySlots == slotsA ? slotsB : slotsA);
 }
